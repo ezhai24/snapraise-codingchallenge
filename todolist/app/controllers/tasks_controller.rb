03578183f97b6f3a404f8963c3289ea2e6eade8e
Emily Zhai <ezhai24@gmail.com>
2017-02-26 19:18:08 -0800

    renders to page on add

--- todolist/app/controllers/tasks_controller.rb
+++ todolist/app/controllers/tasks_controller.rb
@@ -1,16 +1,18 @@
 class TasksController < ApplicationController
     def index
+        @tasks = Task.all
     end
 
     def create
         @task = Task.new(task_params)
 
         @task.save
+        redirect_to :back
     end
 
     private
         def task_params
-            params.require(:task).permit(:task_text)
+            params.require(:task).permit(:taskName)
         end
-
+        
 end
